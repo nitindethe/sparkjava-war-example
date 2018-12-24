920c8227d11f88bd59e89eea9d9cf7e855c2eb6c
nitindethe <34735106+nitindethe@users.noreply.github.com>
2018-12-24 13:41:36 -0600

    123
    123

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -9,6 +9,6 @@ public class HelloWorld implements SparkApplication {
 
 	@Override
 	public void init() {
-		get("/hello", (req, res) -> "Hello Pallav");
+		get("/hello", (req, res) -> "Hello Sachin");
 	}
 }
